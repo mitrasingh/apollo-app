c7db7c556917272f2e0430211c5b7e7942a1cb00
Mitra Singh <mitra.coder@gmail.com>
2023-05-08 00:19:07 -0400

    updated router structure, protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
 
   const currentUser = false
