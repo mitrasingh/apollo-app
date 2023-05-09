db4ee78eb5eefe7de587fec6e61e0ba81306c4f3
Mitra Singh <mitra.coder@gmail.com>
2023-05-08 23:56:42 -0400

    minor update on app/auth setup

--- src/utils/firebase-config.jsx
+++ src/utils/firebase-config.jsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,6 +12,6 @@ const firebaseConfig = {
     measurementId: "G-RLM6XKTT7P"
   };
 
-export const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app);
+
