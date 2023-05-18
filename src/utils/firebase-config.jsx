010f997c5aac9aa7c5ff999e464bcf4c32da4e26
Mitra Singh <mitra.coder@gmail.com>
2023-05-18 07:53:07 -0400

    updated minor updates

--- src/utils/firebase-config.jsx
+++ src/utils/firebase-config.jsx
@@ -12,6 +12,6 @@ const firebaseConfig = {
     measurementId: "G-RLM6XKTT7P"
   };
 
-const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
 
