4795bffbee62a2f8ee8fe3c194d362530ce378a1
Mitra Singh <mitra.coder@gmail.com>
2023-05-18 07:55:40 -0400

    updated firebase code for handleSignUp function

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,30 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { auth } from "../utils/firebase-config"
+import { useState } from "react"
 import { Container, Form, Card, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 export const SignUp = () => {
+
+    const [firstName, setFirstName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+          // eslint-disable-next-line no-unused-vars
+          .then((authUser) => {
+            signInWithEmailAndPassword(auth, email, password).then(
+              updateProfile(auth.currentUser, {
+                displayName: firstName,
+              })
+            );
+          })
+          .catch((err) => {
+            alert(err);
+          });
+      };
 
     return (
         <Container style={{fontSize: "10px", maxWidth: "400px"}} className="mt-4">
@@ -11,12 +34,23 @@ export const SignUp = () => {
                 <p className="d-flex justify-content-center">Already registered?&nbsp; 
                     <Link className="link-primary fw-bold" style={{cursor: "pointer"}} to="/signin">Sign In</Link> 
                 </p>
+
+                <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>First Name</p>
+                <Form.Group className="mb-3" controlId="progress">
+                    <Form.Control 
+                        style={{fontSize: "10px"}} 
+                        type="text" 
+                        placeholder="Enter first name" 
+                        onChange={(e) => setFirstName(e.target.value)}/>
+                </Form.Group>
+
                 <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Email Address</p>
                 <Form.Group className="mb-3" controlId="progress">
                     <Form.Control 
                         style={{fontSize: "10px"}} 
                         type="text" 
-                        placeholder="Enter email" />
+                        placeholder="Enter email" 
+                        onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
 
                 <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Password</p>
@@ -24,14 +58,16 @@ export const SignUp = () => {
                     <Form.Control 
                         style={{fontSize: "10px"}} 
                         type="password" 
-                        placeholder="Enter password" />
+                        placeholder="Enter password" 
+                        onChange={(e) => setPassword(e.target.value)}/>
                 </Form.Group>
 
                 <Button 
                     style={{fontSize: "10px", maxHeight: "30px"}} 
                     variant="primary"  
                     size="sm" 
-                    type="submit">
+                    type="submit"
+                    onClick={handleSignUp}>
                     Sign Up
                 </Button>
 
