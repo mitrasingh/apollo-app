18564c0ca41da84529942782c7463e8082818124
Mitra Singh <mitra.coder@gmail.com>
2023-05-22 00:53:01 -0400

    updated logic to fix user authentication persistance on refresh

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,38 +1,23 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router';
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from "react-firebase-hooks/auth"
+import { Navigate, useLocation } from 'react-router';
 import PropTypes from 'prop-types';
 import { Navigation } from "./Navigation";
+import { LoadAnimation } from "./LoadAnimation"
+
 
 export const ProtectedRoute = ({ children }) => {
+    const location = useLocation();
+    const auth = getAuth();
+    const [user, loading] = useAuthState(auth)
     
-    const user = useSelector((state) => state.user.user);
-    
-    if (!user) {
-        return <Navigate to="/signin" replace />
-    }
-    
-    return (
-        <>
-        <Navigation />
-        {children}
-        </>
-        
-    )
+    if (loading) {
+        return <LoadAnimation />
+    } 
+    return user ? (<><Navigation />{children}</>) : (<Navigate to="/signin" state={{from: location}} />)  
 }
 
 ProtectedRoute.propTypes = {
     children: PropTypes.node
 }
 
-
-// import { Navigation } from './Navigation';
-// import { Home } from '../pages/Home';
-// import { SignIn } from '../pages/SignIn';
-
-    // <>
-    //     {user ? (<><Navigation /> <Home /></>) : <SignIn />}
-    // </>
-
-// import { LoadAnimation } from './LoadAnimation';
-// const isLoading = useSelector((state) => state.data.user.isLoading);
-// {isLoading ? (<LoadAnimation />) : (<>{user ? (<><Navigation /> <Home /></>) : <SignIn />}</>) }
