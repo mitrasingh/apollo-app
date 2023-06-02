ff9773a6c148a1011b9b23786f7d133e8f411820
Mitra Singh <mitra.coder@gmail.com>
2023-06-01 21:36:29 -0400

    updated: added new fields for sign up, also writing user information to database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,6 @@
 import { createUserWithEmailAndPassword, updateProfile, getAuth } from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore"
+import { db } from "../utils/firebase-config"
 import { useState } from "react"
 import { Container, Form, Card, Button, Alert } from "react-bootstrap"
 import { useDispatch } from "react-redux"
@@ -8,10 +10,9 @@ import { loginUser } from "../features/user/userSlice"
 export const SignUp = () => {
 
     const [firstName, setFirstName] = useState("")
-    // const [lastName, setLastName] = useState("")
-    // const [title, setTitle] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [title, setTitle] = useState("")
     const [email, setEmail] = useState("")
-    // const [location, setLocation] = useState("")
     const [password, setPassword] = useState("")
     const [verifyPassword, setVerifyPassword] = useState("")
     
@@ -30,23 +31,29 @@ export const SignUp = () => {
                 await updateProfile(auth.currentUser, {
                     displayName: firstName,
                 })
+                await setDoc(doc(db,"users", `${auth.currentUser.uid}`), {
+                    lastname: lastName,
+                    title: title,
+                })
                 console.log(`User profile updated`)
                 console.log(auth)
                 navigate("/")
             } else {
                 setAlert(true)
-                setAlertMessage("Reason: Passwords do not match")
+                setAlertMessage("Passwords do not match")
             }
         } catch (error) {
             setAlert(true)
-            setAlertMessage(`Reason: ${error.code}`)
+            setAlertMessage(error.code)
             console.log(error.code)
         } finally {
             if (auth) {
                 dispatch(loginUser({
                     userId: auth.currentUser.uid,
                     firstName: auth.currentUser.displayName,
-                    email: auth.currentUser.email
+                    lastName: lastName,
+                    title: title,
+                    email: auth.currentUser.email,
                     }))
             } 
         }
@@ -57,7 +64,7 @@ export const SignUp = () => {
         {alert ? (
                 <Alert variant="danger" onClose={() => setAlert(false)} dismissible>
                     <Alert.Heading>There is an error!</Alert.Heading>
-                    <p>{alertMessage}</p>
+                    <p>Reason: {alertMessage}</p>
                 </Alert>
             ) : ( 
                 null
@@ -80,23 +87,23 @@ export const SignUp = () => {
                             onChange={(e) => setFirstName(e.target.value)}/>
                     </Form.Group>
 
-                    {/* <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Last Name</p>
+                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Last Name</p>
                     <Form.Group className="mb-3">
                         <Form.Control 
                             style={{fontSize: "10px"}} 
                             type="text" 
                             placeholder="Enter last name" 
                             onChange={(e) => setLastName(e.target.value)}/>
-                    </Form.Group> */}
+                    </Form.Group>
 
-                    {/* <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Title</p>
+                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Title</p>
                     <Form.Group className="mb-3" controlId="progress">
                         <Form.Control 
                             style={{fontSize: "10px"}} 
                             type="text" 
                             placeholder="Enter your company title" 
                             onChange={(e) => setTitle(e.target.value)}/>
-                    </Form.Group> */}
+                    </Form.Group>
 
                     <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Email Address</p>
                     <Form.Group className="mb-3">
