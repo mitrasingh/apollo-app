ee6d168661348816ed15b55bb78b887fc7540177
Mitra Singh <mitra.coder@gmail.com>
2023-06-01 21:34:09 -0400

    updated handleLogin to try/catch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { SearchBar } from '../components/SearchBar'
 import { Filter } from '../components/Filter'
 import { useSelector } from 'react-redux'
 
+
 export const Home = () => {
 
   const user = useSelector((state) => state.user.user);
