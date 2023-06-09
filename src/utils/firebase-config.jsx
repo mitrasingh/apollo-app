ba742cf2fce375332299f0013e4f354770c2e4a8
Mitra Singh <mitra.coder@gmail.com>
2023-06-09 10:11:48 -0400

    updated to include firestore storage

--- src/utils/firebase-config.jsx
+++ src/utils/firebase-config.jsx
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
 
 
 const firebaseConfig = {
@@ -10,11 +13,12 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: "G-RLM6XKTT7P"
+    measurementId: "G-RLM6XKTT7P",
   }
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export const storage = getStorage(app)
 
  
