bcba652ff88a772feebb4d1119477a9002e63443
Mitra Singh <mitra.coder@gmail.com>
2023-06-23 09:36:28 -0400

    cleaned up code to remove all code that was no longer needed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import { createUserWithEmailAndPassword, updateProfile, getAuth } from "firebase/auth"
 import { doc, setDoc, getDoc } from "firebase/firestore"
 import { db } from "../utils/firebase-config"
-// import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { useState } from "react"
 import { Container, Form, Card, Button, Alert } from "react-bootstrap"
 import { useDispatch } from "react-redux"
@@ -12,13 +11,11 @@ import { Link, useNavigate } from "react-router-dom"
 export const SignUp = () => {
 
     const [firstName, setFirstName] = useState("")
-    // const [userPhoto, setUserPhoto] = useState(null)
     const [lastName, setLastName] = useState("")
     const [title, setTitle] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [verifyPassword, setVerifyPassword] = useState("")
-    // const [photoURL, setPhotoURL] = useState("")
     
     const [alert, setAlert] = useState(false)
     const [alertMessage, setAlertMessage] = useState("")
@@ -27,9 +24,6 @@ export const SignUp = () => {
     const navigate = useNavigate()
     
     const auth = getAuth()
-
-    // const storage = getStorage()
-    // const storageRef = ref(storage)
     
     const handleSignUp = async (event) => {
         event.preventDefault();
@@ -43,8 +37,6 @@ export const SignUp = () => {
                     lastname: lastName,
                     title: title,
                 })
-                // const imageRef = ref(storageRef, `user-photo/${auth.currentUser.uid}`)
-                // await uploadBytes(imageRef, userPhoto)
 
             } else {
                 setAlert(true)
@@ -52,8 +44,7 @@ export const SignUp = () => {
             }
             const docRef = doc(db, "users", auth.currentUser.uid)
             const docSnap = await getDoc(docRef)
-            // const userPhotoURL = await getDownloadURL(ref(storageRef, `user-photo/${auth.currentUser.uid}`))
-            if (auth && docSnap.exists()) { // userPhotoURL has been removed
+            if (auth && docSnap.exists()) {
                 const data = docSnap.data()
                 dispatch(loginUser({
                     userId: auth.currentUser.uid,
@@ -70,22 +61,6 @@ export const SignUp = () => {
             setAlertMessage(error.code)
         }
     }
-
-    // temporary upload to display photo which allows user to preview avatar
-    // const uploadPhoto = async (e) => {
-    //     e.preventDefault()
-    //     try {
-    //         if (userPhoto == null) return null
-    //         const imageRef = ref(storageRef, "user-photo/temp")
-    //         await uploadBytes(imageRef, userPhoto)
-    //         const getURL = await getDownloadURL(imageRef)
-    //         setPhotoURL(getURL)
-    //     } catch (error) {
-    //         setAlert(true)
-    //         setAlertMessage(error.code)
-    //         console.log(error.code)
-    //     }
-    // }
 
     return (
         <>
@@ -105,46 +80,6 @@ export const SignUp = () => {
                     <p className="d-flex justify-content-center">Already registered?&nbsp; 
                         <Link className="link-primary fw-bold" style={{cursor: "pointer"}} to="/signin">Sign In</Link> 
                     </p>
-
-                    {/* <Row className="mb-4 d-flex justify-content-center">
-                        <Col xs lg="2">
-                            <Stack direction="vertical">
-                                <Image
-                                    style={{
-                                        height: "120px",
-                                        width: "120px",
-                                        objectFit: "cover",
-                                        borderRadius: "50%"
-                                    }}
-                                    src={photoURL === "" ? "src/img/default-profile.png" : photoURL}
-                                    roundedCircle 
-                                />
-                            </Stack>
-                        </Col>
-                    </Row> */}
-                    
-                    {/* <Row className="d-flex justify-content-center">
-                        <Form.Group controlId="formFileSm" className="mb-3">
-                            <Form.Label style={{fontSize: "10px"}}>Square image recommended for better fitment.</Form.Label>
-                            <Form.Control 
-                                type="file" 
-                                size="sm" 
-                                onChange={(event) => setUserPhoto(event.target.files[0])} 
-                            />
-                        </Form.Group>
-                    </Row> */}
-
-                    {/* <Row className="justify-content-center">
-                        <Button 
-                            style={{fontSize: "10px", maxHeight: "30px", maxWidth: "130px"}} 
-                            variant="primary"  
-                            size="sm" 
-                            type="submit"
-                            onClick={uploadPhoto}
-                            >
-                            Upload Photo
-                        </Button>
-                    </Row> */}
 
                     <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>First Name</p>
                     <Form.Group className="mb-3">
