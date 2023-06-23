414319766c2ebcc5bcc54695ac64a8c210f5f046
Mitra Singh <mitra.coder@gmail.com>
2023-06-23 09:34:14 -0400

    cleaned up code and removed redux state which was not needed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import { TaskCard } from '../components/TaskCard'
 import { SearchBar } from '../components/SearchBar'
 import { Filter } from '../components/Filter'
-import { useSelector } from 'react-redux'
 
 
 export const Home = () => {
 
-  const user = useSelector((state) => state.user.user);
-  console.log(user)
   return (
     <>
         <SearchBar />
