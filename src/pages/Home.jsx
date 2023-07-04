2ecca64d2f57e3e4cb12869d609dd52e8319dcba
Mitra Singh <mitra.coder@gmail.com>
2023-07-03 22:08:12 -0400

    tasks that have been entered in database are now displayed on home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,46 @@
 import { TaskCard } from '../components/TaskCard'
 import { SearchBar } from '../components/SearchBar'
 import { Filter } from '../components/Filter'
-
+import { useEffect, useState } from 'react'
+import { collection, getDocs, query } from 'firebase/firestore'
+import { db } from '../utils/firebase-config'
+// import { collection, query, getDocs } from "firebase/firestore"
+// import { db } from "../utils/firebase-config"
+// import { useEffect } from 'react'
 
 export const Home = () => {
 
+  const [tasks, setTasks] = useState([])
+  const queryTasks = query(collection(db, "tasks"))
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const data = await getDocs(queryTasks)
+      setTasks(data.docs.map((doc) => ({...doc.data(), taskId: doc.id})))
+    }
+    getTasks()
+  }, [])
+  
   return (
     <>
-        <SearchBar />
-        <Filter />
-        <TaskCard />
+      <SearchBar />
+      <Filter />
+      {tasks.map((task) => {
+        return (
+          <>
+            <TaskCard 
+              descriptionTask={task.descriptionTask}
+              dueDate={task.dueDate}
+              priorityLevel={task.priorityLevel}
+              statusProject={task.statusProject}
+              taskId={task.taskId}
+              taskName={task.taskName}
+              userId={task.userId}       
+            />
+          </>
+        )
+      })}
     </>
   )
 } 
+
