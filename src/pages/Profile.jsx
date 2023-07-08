6dd4edbdc1da557c24a6d153361ca0fd80a999cb
Mitra Singh <mitra.coder@gmail.com>
2023-07-08 08:47:12 -0400

    added upload field for user image and added usestate functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react'
 import { Button, Col, Container, Form, Row, Stack, Image } from 'react-bootstrap'
 import { useSelector } from "react-redux"
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
+
 
 export const Profile = () => {
 
     const user = useSelector((state) => state.user)
+
+    const [userUpdatedPhoto, setUserUpdatedPhoto] = useState("") //allows user to see how photo is displayed before upload
+    const [photoURL, setPhotoURL] = useState("") 
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [title, setTitle] = useState("")
+    const [email, setEmail] = useState("")
+
+    const storage = getStorage()
+    const storageRef = ref(storage)
+
+    const setPhotoHandle = async (e) => {
+        e.preventDefault()
+        try {
+            const imageRef = ref(storageRef, "user-photo/temp")
+            await uploadBytes(imageRef, userUpdatedPhoto)
+            const getURL = await getDownloadURL(imageRef)
+            setPhotoURL(getURL)
+        } catch (error) {
+            console.log(error.code)
+        }
+    }
+
 
     return (
         <>
@@ -18,14 +44,25 @@ export const Profile = () => {
                                 objectFit: "cover",
                                 borderRadius: "50%"
                             }} 
-                            src={user.userPhoto}
+                            // src={user.userPhoto}
+                            src={photoURL === "" ? user.userPhoto : photoURL}
                             roundedCircle />
+
+                        <Form.Group>
+                            <Form.Control 
+                                type="file"
+                                size="sm"
+                                onChange={(event) => setUserUpdatedPhoto(event.target.files[0])}
+                            />
+                        </Form.Group>
+
                         <Button 
                             style={{fontSize: "8px", maxHeight: "30px", maxWidth: "75px"}} 
                             className="ms-5 mt-2" 
                             variant="secondary" 
                             size="sm" 
-                            type="submit">Update Photo
+                            type="file"
+                            onClick={setPhotoHandle}>Set Photo
                         </Button>
                     </Stack>
                 </Col>
@@ -35,19 +72,35 @@ export const Profile = () => {
                 <Stack>
                     <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>First Name</p>
                     <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control style={{fontSize: "10px"}} type="text" placeholder={user.firstName} />
+                        <Form.Control 
+                            style={{fontSize: "10px"}} 
+                            type="text" 
+                            value={user.firstName}
+                            onChange={(e) => setFirstName(e.target.value)} />
                     </Form.Group>
+                    
                     <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>Last Name</p>
                     <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control style={{fontSize: "10px"}} type="text" placeholder={user.lastName} />
+                        <Form.Control 
+                            style={{fontSize: "10px"}} 
+                            type="text" 
+                            value={user.lastName} />
                     </Form.Group>
+                    
                     <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>Title</p>
                     <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control style={{fontSize: "10px"}} type="text" placeholder={user.title} />
+                        <Form.Control 
+                            style={{fontSize: "10px"}} 
+                            type="text" 
+                            value={user.title} />
                     </Form.Group>
+                   
                     <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>E-mail</p>
                     <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control style={{fontSize: "10px"}} type="text" placeholder={user.email} />
+                        <Form.Control 
+                            style={{fontSize: "10px"}} 
+                            type="text" 
+                            value={user.email} />
                     </Form.Group>
                 </Stack>
             </Row>
