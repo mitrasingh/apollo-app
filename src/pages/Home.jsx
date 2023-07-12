c31389a113174e18fd5714829d52971f40952f9c
Mitra Singh <mitra.coder@gmail.com>
2023-07-11 22:51:06 -0400

    added a refresh button for user to refresh task list when needed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import { TaskCard } from '../components/TaskCard'
 import { SearchBar } from '../components/SearchBar'
 import { Filter } from '../components/Filter'
+import { Refresh } from '../components/Refresh'
 import { useEffect, useState } from 'react'
 import { collection, getDocs, query } from 'firebase/firestore'
 import { db } from '../utils/firebase-config'
+import { Row, Col, Container } from 'react-bootstrap'
 
 export const Home = () => {
 
   const [tasks, setTasks] = useState([])
+  const [refresh, setRefresh] = useState(false)
 
   // pulling data from database and mapping each task into the tasks variable
   useEffect(() => {
@@ -16,12 +19,21 @@ export const Home = () => {
       setTasks(data.docs.map((doc) => ({...doc.data(), taskId: doc.id})))
     }
     getTasks()
-  },[])
+  },[]) 
 
   return (
     <>
       <SearchBar />
-      <Filter />
+      <Container className="mt-2">
+        <Row>
+          <Col xs lg="1">
+            <Filter />      
+          </Col>
+          <Col className="mt-1 px-3">
+            <Refresh />
+          </Col>
+        </Row>
+      </Container>
 
       {/* mapping each task from tasks variable as per TaskCard */}
       {tasks.map((task) => {
