3ffba0dab7d6658283c86b74556d002418446f02
Mitra Singh <mitra.coder@gmail.com>
2023-07-13 19:13:47 -0400

    updated filter button, added subcategories

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -10,11 +10,19 @@ export const Filter = () => {
         <Dropdown.Toggle style={{maxHeight: "20px"}} className="d-flex align-items-center" split variant="dark" id="dropdown-split-basic" />
 
         <Dropdown.Menu style={{fontSize: "10px"}}>
-            <Dropdown.Item as={Link} to="">by Newest</Dropdown.Item>
+            <Dropdown.Item >by Newest</Dropdown.Item>
             <Dropdown.Item as={Link} to="">by Oldest</Dropdown.Item>
-            <Dropdown.Item as={Link} to="">by Priority Level</Dropdown.Item>
+            <Dropdown.Item>by Priority Level</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">In Progress</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">Done</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">On Hold</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">Cancelled</Dropdown.Item>
             <Dropdown.Item as={Link} to="">by Status</Dropdown.Item>
-        </Dropdown.Menu>
+              <Dropdown.Item as={Link} to="" className="ms-3">Urgent</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">High</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">Medium</Dropdown.Item>
+              <Dropdown.Item as={Link} to="" className="ms-3">Low</Dropdown.Item>
+        </Dropdown.Menu> 
         </Dropdown>   
     </Container>
   )
