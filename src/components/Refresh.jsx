cf0565df78dea7d3df8bc8222b94a5043fbd7f8e
Mitra Singh <mitra.coder@gmail.com>
2023-07-12 21:59:43 -0400

    added prop to component which refreshes tasklist as useeffect dependency

--- src/components/Refresh.jsx
+++ src/components/Refresh.jsx
@@ -1,9 +1,20 @@
 import { Button, Container } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
-export const Refresh = () => {
+export const Refresh = ({ refreshTasksHandle }) => {
   return (
     <Container>
-        <Button style={{fontSize: "9px", maxHeight: "20px"}} className="d-flex align-items-center" variant="dark">Refresh Tasks</Button>
+        <Button 
+            style={{fontSize: "9px", maxHeight: "20px"}} 
+            className="d-flex align-items-center" 
+            variant="dark"
+            onClick={refreshTasksHandle}>
+                Refresh Tasks
+        </Button>
     </Container>
   )
+}
+
+Refresh.propTypes = {
+    refreshTasksHandle: PropTypes.func
 }
