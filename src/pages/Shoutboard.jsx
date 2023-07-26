51f94bef1e227c224361a4c3b5c18b30cfc25cfe
Mitra Singh <mitra.coder@gmail.com>
2023-07-25 23:13:04 -0400

    changed the layout of Shoutboard page, coded initial first thread

--- src/pages/Shoutboard.jsx
+++ src/pages/Shoutboard.jsx
@@ -1,15 +1,15 @@
-import { Card, Col, Container, Form, Row, Stack } from 'react-bootstrap'
+import { Card, Col, Container, Row, Image } from 'react-bootstrap'
 
 export const Shoutboard = () => {
   return (
     <>
     <Container className="mt-4">
-      <Card>
+      <Card style={{maxHeight: "65px"}} >
         <Card.Body>
-          <Row className="px-2 py-2">
+          <Row>
 
             <Col xs lg="1" className="d-flex justify-content-end">
-              <img
+              <Image
                 src="src/img/default-profile.png"
                 width="35"
                 height="35"
@@ -18,47 +18,29 @@ export const Shoutboard = () => {
               />
             </Col>
 
-            <Col>
-              <Row>
-                <Stack direction="horizontal">
-                  <p style={{fontSize: "12px"}} className="fw-bold">UserName</p>
-                  <p style={{fontSize: "8px"}} className="ms-2">12:05PM</p>
-                </Stack>
+            <Col xs lg="9">
+              <Row style={{fontSize: "13px"}} className="fw-bold">
+                <Col xs lg="5">Topic Name</Col>
               </Row>
-              <Row style={{fontSize: "10px"}}>
-                <p>Message will appear in this area. Character limit to be determined.</p>
+              <Row style={{fontSize: "9px"}}>
+                <Col xs lg="5">by Username</Col>
               </Row>
-              <img
-                src="src/img/thumbs-dark.png"
-                width="20"
-                height="20"
-                className="align-top"
-                alt="Apollo Logo"
-              />
             </Col>
-            <hr className="mt-2"></hr>
+
+            <Col className="mt-2 d-flex">
+              <Image
+                  src="src/img/comment-icon.png"
+                  width="20"
+                  height="20"
+                  className="d-inline-block align-top"
+                  alt="Apollo Logo"
+              />
+              <p style={{fontSize: "9px"}} className="mt-1 ms-2" >3 Replies</p>
+            </Col>
+
           </Row>
         </Card.Body>
       </Card>
-
-      <Form className="mt-3">
-        <Row>
-        <Col xs lg="11">
-          <Form.Group className="mb-3" controlId="sendMessage">
-            <Form.Control style={{fontSize: "10px"}} type="text" placeholder="Type something..." />
-          </Form.Group>
-        </Col>
-
-        <Col xs lg="1">
-          <img
-                src="src/img/send.png"
-                width="20"
-                height="20"
-                alt="Send message icon"
-          />
-        </Col>
-        </Row>
-      </Form>
     </Container>
     </>
   )
