e86fba930e1d7182d3678176646e92e5a05ec437
Mitra Singh <mitra.coder@gmail.com>
2023-07-26 22:38:08 -0400

    components added to page

--- src/pages/Shoutboard.jsx
+++ src/pages/Shoutboard.jsx
@@ -1,9 +1,14 @@
+import { CreateTopicButton } from "../components/CreateTopicButton"
 import { TopicCard } from "../components/TopicCard"
+import { Container } from "react-bootstrap"
+import { CreateTopicForm } from "../components/CreateTopicForm"
 
 export const Shoutboard = () => {
   return (
-    <>
-      <TopicCard />
-    </>
+      <Container className="mt-4">
+        <CreateTopicButton />
+        <CreateTopicForm />
+        <TopicCard />        
+      </Container>
   )
 }
