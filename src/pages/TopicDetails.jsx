aef0351fc5b3afb77814cd2aab1675b39e0dfafe
Mitra Singh <mitra.coder@gmail.com>
2023-07-31 16:10:36 -0400

    updated to include useParams via react router, data is now fetched and displayed for specific topic

--- src/pages/TopicDetails.jsx
+++ src/pages/TopicDetails.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../utils/firebase-config"
+
 
 export const TopicDetails = () => {
 
     const { id } = useParams()
 
+    const [topic, setTopic] = useState({})
+
+    useEffect(() => {
+        const fetchTopic = async () => {
+            try {
+                const docRef = doc(db,"topics",id)
+                const docSnap = await getDoc(docRef)
+                if (docSnap.exists()) {
+                    const data = docSnap.data()
+                    setTopic(data)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchTopic()
+    },[])
+
+    console.log(topic)
+
     return (
-        <div>Topic: {id}</div>
+        <>
+            <div>Title: {topic.title}</div>
+            <div>Description: {topic.description}</div>
+            <div>Name: {topic.firstName} {topic.lastName}</div>
+            <div>userId: {topic.userId}</div>
+        </>
     )
 }
-
-// NEED TO ADD LOADER FUNCTION
 
