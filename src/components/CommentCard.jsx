4a1bbb0525d33da577d5ef931328256c79812a9d
Mitra Singh <mitra.coder@gmail.com>
2023-08-03 23:48:57 -0400

    structured component of where information should be displayed

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -5,7 +5,7 @@ import { Container, Row, Col, Stack, Image, Card } from "react-bootstrap"
 export const CommentCard = () => {
   return (
     <Container className="mt-4">
-        <Card>
+        <Card style={{padding: "10px"}}>
         <Row>
             <Col>
                 <Stack direction="horizontal" gap={3}>
@@ -19,7 +19,18 @@ export const CommentCard = () => {
                         src="src/img/default-profile.png"
                         roundedCircle 
                     />
+                    <p style={{fontSize:"9px", marginTop:"12px"}}>First Last Name</p>
+                    <p style={{fontSize:"8px", marginTop:"12px"}}>Timestamp</p>
                 </Stack>
+                
+                <Stack className="mt-2">
+                    <Row>
+                        <Col>
+                            <p style={{fontSize:"11px"}}>comment dialogue</p>
+                        </Col>
+                    </Row>
+                </Stack>
+
             </Col>
         </Row>
         </Card>
