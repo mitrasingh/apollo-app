c3cc4b54afcd420b5e501a040dd95223efdef227
Mitra Singh <mitra.coder@gmail.com>
2023-08-04 23:22:50 -0400

    created a function that converts firebase timestamp to a readable date

---
+++ src/utils/format-date.jsx
@@ -0,0 +1,8 @@
+function formatDate(date) {
+    const formatDate = new Date(
+        date.seconds * 1000 + date.nanoseconds / 1000000
+    );
+    return formatDate.toLocaleTimeString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+}
+
+export default formatDate
