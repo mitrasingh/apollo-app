512f06a1c2b191b8644646123aead0a4773d4cf9
Mitra Singh <mitra.coder@gmail.com>
2023-08-05 22:20:27 -0400

    added date posted to the component

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,39 +1,53 @@
-
 import { Container, Row, Col, Stack, Image, Card } from "react-bootstrap"
+import PropTypes from 'prop-types';
+import formatDate from ".././utils/format-date"
 
 
-export const CommentCard = () => {
-  return (
-    <Container className="mt-4">
-        <Card style={{padding: "10px"}}>
-        <Row>
-            <Col>
-                <Stack direction="horizontal" gap={3}>
-                    <Image
-                        style={{
-                            height: "25px",
-                            width: "25px",
-                            objectFit: "cover",
-                            borderRadius: "50%"
-                        }} 
-                        src="src/img/default-profile.png"
-                        roundedCircle 
-                    />
-                    <p style={{fontSize:"9px", marginTop:"12px"}}>First Last Name</p>
-                    <p style={{fontSize:"8px", marginTop:"12px"}}>Timestamp</p>
-                </Stack>
-                
-                <Stack className="mt-2">
-                    <Row>
-                        <Col>
-                            <p style={{fontSize:"11px"}}>comment dialogue</p>
-                        </Col>
-                    </Row>
-                </Stack>
+export const CommentCard = ( props ) => {
+    
+    const { userPhoto, firstName, lastName, userComment, datePosted } = props.comment
 
-            </Col>
-        </Row>
-        </Card>
-    </Container>
-  )
+    return (
+        <Container className="mt-4">
+            <Card style={{padding: "10px"}}>
+            <Row>
+                <Col>
+                    <Stack direction="horizontal" gap={2}>
+                        <Image
+                            style={{
+                                height: "25px",
+                                width: "25px",
+                                objectFit: "cover",
+                                borderRadius: "50%"
+                            }} 
+                            src={userPhoto}
+                            roundedCircle 
+                        />
+                        <p style={{fontSize:"9px", marginTop:"12px"}}>{firstName} {lastName}</p>
+                        <p style={{fontSize:"8px", marginTop:"12px"}}>posted at: {formatDate(datePosted)}</p>
+                    </Stack>
+                    
+                    <Stack className="mt-2">
+                        <Row>
+                            <Col>
+                                <p style={{fontSize:"11px"}}>{userComment}</p>
+                            </Col>
+                        </Row>
+                    </Stack>
+
+                </Col>
+            </Row>
+            </Card>
+        </Container>
+    )
+}
+
+CommentCard.propTypes = {
+    comment: PropTypes.any,
+    userId: PropTypes.string,
+    userPhoto: PropTypes.string,
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    userComment: PropTypes.string,
+    datePosted: PropTypes.string
 }
