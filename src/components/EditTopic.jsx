8e3d3182605978b97ccfe500b1eca8dfc3e1ec47
Mitra Singh <mitra.coder@gmail.com>
2023-08-24 19:55:33 -0400

    updated all prop names

--- src/components/EditTopic.jsx
+++ src/components/EditTopic.jsx
@@ -1,76 +1,80 @@
-import { Form, Stack, Button } from "react-bootstrap"
-import PropTypes from "prop-types"
-import { useState } from "react"
-import { db } from '../utils/firebase-config'
-import { doc, updateDoc } from 'firebase/firestore'
+import { Form, Stack, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { db } from "../utils/firebase-config";
+import { doc, updateDoc } from "firebase/firestore";
 
-export const EditTopic = ({ setIsEditTopic, description, id, setTopicRefresh }) => {
+export const EditTopic = ({
+	setIsEditTopicDisplayed,
+	description,
+	id,
+	setIsTopicRefreshed,
+}) => {
+	// state stores the user data in the form field
+	const [userInput, setUserInput] = useState(description);
 
-    // state stores the user data in the form field
-    const [userInput, setUserInput] = useState(description)
-    
-    // function updates the topic's description and refreshes the topic data for immediate update
-    const handleUpdateButton = async (e) => {
-        e.preventDefault()
-        try {
-            await updateDoc(doc(db,"topics",id), {
-                description: userInput
-            })
-            if (updateDoc) {
-                setIsEditTopic(false)
-                setTopicRefresh((current) => !current)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
+	// function updates the topic's description and refreshes the topic data for immediate update
+	const handleUpdateButton = async (e) => {
+		e.preventDefault();
+		try {
+			await updateDoc(doc(db, "topics", id), {
+				description: userInput,
+			});
+			if (updateDoc) {
+				setIsEditTopicDisplayed(false);
+				setIsTopicRefreshed((current) => !current);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    return (
-        <>
-            <Form className="mt-4">
-                <Form.Group className="mb-3" controlId="description">
-                    <Form.Control 
-                        style={{fontSize: "10px"}} 
-                        maxLength={100000}
-                        rows={5}
-                        type="text" 
-                        as="textarea"
-                        placeholder="What are your thoughts?"
-                        value={userInput}
-                        onChange={(e) => setUserInput(e.target.value)}
-                    />
-                </Form.Group>
-            </Form>
+	return (
+		<>
+			<Form className="mt-4">
+				<Form.Group className="mb-3" controlId="description">
+					<Form.Control
+						style={{ fontSize: "10px" }}
+						maxLength={100000}
+						rows={5}
+						type="text"
+						as="textarea"
+						placeholder="What are your thoughts?"
+						value={userInput}
+						onChange={(e) => setUserInput(e.target.value)}
+					/>
+				</Form.Group>
+			</Form>
 
-            <Stack direction="horizontal" gap={1}>
-            <Button 
-                    style={{fontSize: "10px", maxHeight: "30px", minWidth:"40px"}} 
-                    className="ms-2" 
-                    variant="dark" 
-                    size="sm" 
-                    type="submit"
-                    onClick={() => setIsEditTopic(false)}
-                    >
-                        Cancel
-                </Button>
-                <Button 
-                    style={{fontSize: "10px", maxHeight: "30px", minWidth:"40px"}} 
-                    className="ms-2" 
-                    variant="dark" 
-                    size="sm" 
-                    type="submit"
-                    onClick={handleUpdateButton}
-                    >
-                        Update
-                </Button> 
-            </Stack>
-        </>
-    )
-}
+			<Stack direction="horizontal" gap={1}>
+				<Button
+					style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
+					className="ms-2"
+					variant="dark"
+					size="sm"
+					type="submit"
+					onClick={() => setIsEditTopicDisplayed(false)}
+				>
+					Cancel
+				</Button>
+				<Button
+					style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
+					className="ms-2"
+					variant="dark"
+					size="sm"
+					type="submit"
+					onClick={handleUpdateButton}
+				>
+					Update
+				</Button>
+			</Stack>
+		</>
+	);
+};
 
 EditTopic.propTypes = {
-    description: PropTypes.string,
-    id: PropTypes.string,
-    setIsEditTopic: PropTypes.func,
-    setTopicRefresh: PropTypes.func
-}
+	description: PropTypes.string,
+	id: PropTypes.string,
+	setIsEditTopicDisplayed: PropTypes.func,
+	setIsTopicRefreshed: PropTypes.func,
+};
