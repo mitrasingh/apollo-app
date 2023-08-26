9937c86f2f8785a2ab64492aad2e0668406d6df4
Mitra Singh <mitra.coder@gmail.com>
2023-08-25 23:06:07 -0400

    cleaned up code, restructured code for better readability and visual consistency

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,37 +3,36 @@ import PropTypes from 'prop-types';
 
 export const SearchBar = ({ userInputSearchBar }) => {
 
-    // sending user form value to parent (Home.jsx)
+    // sending input form value to parent (Home.jsx)
     const handleUserInput = (e) => {
-            const userInput = e.target.value
-            userInputSearchBar(userInput)
-            console.log(e.target.value == 0 ? 'true' : 'false' )
+        const userInput = e.target.value
+        userInputSearchBar(userInput)
     }
 
     return (
         <Container className="mt-4">
             <Row className="justify-content-center">
                 <Col sm={5}>
-                <Form className="d-flex">
-                    <InputGroup>
-                        <InputGroup.Text className="bg-white">
-                            <img
-                            src="public/img/search.svg"
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                            alt="Search bar button"
+                    <Form className="d-flex">
+                        <InputGroup>
+                            <InputGroup.Text className="bg-white">
+                                <img
+                                    src="public/img/search.svg"
+                                    width="30"
+                                    height="30"
+                                    className="d-inline-block align-top"
+                                    alt="Search bar button"
+                                />
+                            </InputGroup.Text>
+                            <FormControl
+                                style={{ fontSize: "11px" }}
+                                type="text"
+                                className="me-2"
+                                placeholder="Search by task name..."
+                                onChange={handleUserInput}
                             />
-                        </InputGroup.Text>
-                        <FormControl 
-                            style={{fontSize: "11px"}} 
-                            type="text" 
-                            className="me-2" 
-                            placeholder="Search by task name..."
-                            onChange={handleUserInput}
-                        />
-                    </InputGroup>                
-                </Form>
+                        </InputGroup>
+                    </Form>
                 </Col>
             </Row>
         </Container>
