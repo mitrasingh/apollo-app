6d9d126adc8e8b20fd9527b33130f3102a92b4c8
Mitra Singh <mitra.coder@gmail.com>
2023-08-29 00:58:27 -0400

    vast changes made to make code shorter, cleaner and more readable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,87 +10,63 @@ import { Row, Col, Container } from "react-bootstrap";
 export const Home = () => {
 	// initial state for task data from database
 	const [tasks, setTasks] = useState([]);
-
-	// result of tasks that have been filtered by options from filterbutton (Filter.jsx)
-	const [filteredTasks, setFilteredTasks] = useState([]);
-
-	// displays which array of tasks should be shown
-	const [displaySorted, setDisplaySorted] = useState(false);
-	const [displayFiltered, setDisplayFiltered] = useState(false);
-	const [displaySearched, setDisplaySearched] = useState(false);
+	const [tasksFiltered, setTasksFiltered] = useState([])
+	// const [tasksSearched, setTasksSearched] = useState([])
 
 	//user input for SearchBar
 	const [userInput, setUserInput] = useState("");
 
-	// refreshes the tasks state which retrieve any new tasks from database
-	const [refresh, setRefresh] = useState(false);
-
 	// pulling data from database and mapping each task into the tasks variable
+	const fetchTasks = async () => {
+		try {
+			const data = await getDocs(query(collection(db, "tasks")));
+			setTasks(data.docs.map((doc) => ({ ...doc.data(), taskId: doc.id })));
+			setTasksFiltered(data.docs.map((doc) => ({ ...doc.data(), taskId: doc.id })));
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	useEffect(() => {
-		const getTasks = async () => {
-			try {
-				const data = await getDocs(query(collection(db, "tasks")));
-				setTasks(data.docs.map((doc) => ({ ...doc.data(), taskId: doc.id })));
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getTasks();
-		setRefresh(false);
-		console.log("api retrieval");
-	}, [refresh]);
+		fetchTasks()
+	}, [])
 
 	// refreshes tasks state by retrieving any new data from database, clears displays to original list, clears user search value
 	const refreshTasksHandle = () => {
-		setRefresh(true);
-		setUserInput("");
-		setDisplaySorted(false);
-		setDisplayFiltered(false);
-		setDisplaySearched(false);
+		fetchTasks()
 	};
 
 	// receiving user input from SearchBar component
 	const userInputSearchBar = (formInput) => {
 		setUserInput(formInput);
-		setDisplaySearched(true);
 	};
 
 	// filter options fuctionality for the dropdown filter button
 	const filterNewestHandle = () => {
-		setTasks(
-			[...tasks].sort((a, b) => new Date(b.dueDate) - new Date(a.dueDate))
-		);
-		setDisplaySorted(true);
-		setDisplayFiltered(false);
-		setDisplaySearched(false);
+		const sortNew = [...tasks].sort((a, b) => new Date(b.dueDate) - new Date(a.dueDate))
+		setTasksFiltered(sortNew)
 	};
 
 	const filterOldestHandle = () => {
-		setTasks(
-			[...tasks].sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
-		);
-		setDisplaySorted(true);
-		setDisplayFiltered(false);
-		setDisplaySearched(false);
+		const sortOld = [...tasks].sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
+		setTasksFiltered(sortOld)
 	};
 
 	const filterPriorityHandle = (priority) => {
-		setFilteredTasks(tasks.filter((task) => task.priorityLevel === priority));
-		setDisplayFiltered(true);
-		setDisplaySorted(false);
-		setDisplaySearched(false);
+		setTasksFiltered(tasks.filter((task) => task.priorityLevel === priority));
 	};
 
 	const filterStatusHandle = (status) => {
-		setFilteredTasks(tasks.filter((task) => task.statusProject === status));
-		setDisplayFiltered(true);
-		setDisplaySorted(false);
-		setDisplaySearched(false);
+		setTasksFiltered(tasks.filter((task) => task.statusProject === status));
 	};
+
+	const filterSearchHandle = () => {
+		setTasksFiltered(tasks.filter((task) => task.taskName.toLowerCase().includes(userInput.toLowerCase())));
+	}
 
 	return (
 		<>
-			<SearchBar userInputSearchBar={userInputSearchBar} />
+			<SearchBar userInputSearchBar={userInputSearchBar} filterSearchHandle={filterSearchHandle} />
 			<Container className="mt-2">
 				<Row>
 					<Col xs lg="1">
@@ -104,77 +80,23 @@ export const Home = () => {
 					<Col xs lg="2" className="mt-1 px-3">
 						<RefreshButton
 							refreshTasksHandle={refreshTasksHandle}
-							displayFiltered={displayFiltered}
-							displaySorted={displaySorted}
+							filterSearchHandle={filterSearchHandle}
 						/>
 					</Col>
 				</Row>
 
-				{/* initial display that is shown */}
-				{!displayFiltered && !displaySorted && userInput === ""
-					? tasks.map((task) => {
-							return (
-								<TaskCard
-									refreshTasksHandle={refreshTasksHandle}
-									task={task}
-									key={task.taskId}
-								/>
-							);
-					  })
-					: null}
+				{tasksFiltered.length === 0 && <p className="mt-4 d-flex justify-content-center">No tasks found</p>}
 
-				{/* if user inputs any value into search bar */}
-				{displaySearched
-					? tasks
-							.filter((task) => {
-								return userInput.toLowerCase() === ""
-									? null
-									: task.taskName.toLowerCase().includes(userInput);
-							})
-							.map((task) => (
-								<TaskCard
-									refreshTasksHandle={refreshTasksHandle}
-									task={task}
-									key={task.taskId}
-								/>
-							))
-					: null}
+				{tasksFiltered.map((task) => {
+					return (
+						<TaskCard
+							refreshTasksHandle={refreshTasksHandle}
+							task={task}
+							key={task.taskId}
+						/>
+					)
+				})}
 
-				{/* if filtered options (priority level or status) is clicked via filter button */}
-				{displayFiltered
-					? filteredTasks.map((task) => {
-							return (
-								<TaskCard
-									refreshTasksHandle={refreshTasksHandle}
-									task={task}
-									key={task.taskId}
-								/>
-							);
-					  })
-					: null}
-
-				{/* if filtered options (priority levl or status) is clicked and no tasks match criteria */}
-				{displayFiltered && filteredTasks.length == 0 ? (
-					<h5>No tasks to display</h5>
-				) : null}
-
-				{/* if sorted options (newest or oldest) is clicked via filter button */}
-				{displaySorted
-					? tasks.map((task) => {
-							return (
-								<TaskCard
-									refreshTasksHandle={refreshTasksHandle}
-									task={task}
-									key={task.taskId}
-								/>
-							);
-					  })
-					: null}
-
-				{/* if sorted options (newest or oldest) is clicked and no tasks match criteria */}
-				{displaySorted && tasks.length == 0 ? (
-					<h5>No tasks to display</h5>
-				) : null}
 			</Container>
 		</>
 	);
