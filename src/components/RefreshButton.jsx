3b2b09609d125c9b7e6c1650b6a0770770be2783
Mitra Singh <mitra.coder@gmail.com>
2023-08-29 21:51:45 -0400

    prop added for dynamic button text

--- src/components/RefreshButton.jsx
+++ src/components/RefreshButton.jsx
@@ -1,22 +1,21 @@
 import { Button, Container } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
-export const RefreshButton = ({ refreshTasksHandle, displayFiltered, displaySorted }) => {
+export const RefreshButton = ({ refreshTasksHandle, isClearFilterDisplayed }) => {
   return (
     <Container>
-        <Button 
-            style={{fontSize: "9px", maxHeight: "20px"}} 
-            className="d-flex align-items-center" 
-            variant="dark"
-            onClick={refreshTasksHandle}>
-                {displayFiltered || displaySorted ? "Clear Filters" : "Refresh Tasks"}
-        </Button>
+      <Button
+        style={{ fontSize: "9px", maxHeight: "20px" }}
+        className="d-flex align-items-center"
+        variant="dark"
+        onClick={refreshTasksHandle}>
+        {isClearFilterDisplayed ? "Clear Filters" : "Refresh Tasks"}
+      </Button>
     </Container>
   )
 }
 
 RefreshButton.propTypes = {
-    refreshTasksHandle: PropTypes.func,
-    displayFiltered: PropTypes.bool,
-    displaySorted: PropTypes.bool
+  refreshTasksHandle: PropTypes.func,
+  isClearFilterDisplayed: PropTypes.bool
 }
