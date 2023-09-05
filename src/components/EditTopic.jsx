b2ebfeb877f2b92e6849e788a3524ca5c63e241a
Mitra Singh <mitra.coder@gmail.com>
2023-09-05 08:05:02 -0400

    converted component to react-form-hook

--- src/components/EditTopic.jsx
+++ src/components/EditTopic.jsx
@@ -1,24 +1,25 @@
 import { Form, Stack, Button } from "react-bootstrap";
 import PropTypes from "prop-types";
-import { useState } from "react";
 import { db } from "../utils/firebase-config";
 import { doc, updateDoc } from "firebase/firestore";
+import { useForm } from "react-hook-form";
 
-export const EditTopic = ({
-	setIsEditTopicDisplayed,
-	description,
-	id,
-	setIsTopicRefreshed,
-}) => {
-	// state stores the user data in the form field
-	const [userInput, setUserInput] = useState(description);
+export const EditTopic = ({ setIsEditTopicDisplayed, description, id, setIsTopicRefreshed, }) => {
+
+	const form = useForm({
+		defaultValues: {
+			newdescription: description
+		}
+	});
+	const { register, handleSubmit, formState } = form;
+	const { errors } = formState;
 
 	// function updates the topic's description and refreshes the topic data for immediate update
-	const handleUpdateButton = async (e) => {
-		e.preventDefault();
+	const onSubmit = async (data) => {
+		console.log("submitted")
 		try {
 			await updateDoc(doc(db, "topics", id), {
-				description: userInput,
+				description: data.newdescription,
 			});
 			if (updateDoc) {
 				setIsEditTopicDisplayed(false);
@@ -31,43 +32,47 @@ export const EditTopic = ({
 
 	return (
 		<>
-			<Form className="mt-4">
-				<Form.Group className="mb-3" controlId="description">
+			<Form className="mt-4" onSubmit={handleSubmit(onSubmit)} noValidate>
+				<Form.Group className="mb-3">
 					<Form.Control
 						style={{ fontSize: "10px" }}
 						maxLength={100000}
 						rows={5}
 						type="text"
 						as="textarea"
-						placeholder="What are your thoughts?"
-						value={userInput}
-						onChange={(e) => setUserInput(e.target.value)}
+						id="newdescription"
+						{...register("newdescription", {
+							required: {
+								value: true,
+								message: "Description of task is required!"
+							}
+						})}
 					/>
+					<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.newdescription?.message}</p>
 				</Form.Group>
-			</Form>
 
-			<Stack direction="horizontal" gap={1}>
-				<Button
-					style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
-					className="ms-2"
-					variant="dark"
-					size="sm"
-					type="submit"
-					onClick={() => setIsEditTopicDisplayed(false)}
-				>
-					Cancel
-				</Button>
-				<Button
-					style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
-					className="ms-2"
-					variant="dark"
-					size="sm"
-					type="submit"
-					onClick={handleUpdateButton}
-				>
-					Update
-				</Button>
-			</Stack>
+
+				<Stack direction="horizontal" gap={1}>
+					<Button
+						style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
+						className="ms-2"
+						variant="dark"
+						size="sm"
+						onClick={() => setIsEditTopicDisplayed(false)}
+					>
+						Cancel
+					</Button>
+					<Button
+						style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
+						className="ms-2"
+						variant="dark"
+						size="sm"
+						type="submit"
+					>
+						Update
+					</Button>
+				</Stack>
+			</Form>
 		</>
 	);
 };
