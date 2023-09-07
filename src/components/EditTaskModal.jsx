579f468fe97e2d8422dd7901ef15a1996b717c7b
Mitra Singh <mitra.coder@gmail.com>
2023-09-07 18:28:38 -0400

    component updated to react hook form

--- src/components/EditTaskModal.jsx
+++ src/components/EditTaskModal.jsx
@@ -1,23 +1,18 @@
 import { Button, Form, Modal, Stack, Image } from "react-bootstrap";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { db } from "../utils/firebase-config";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useForm } from "react-hook-form"
 
 // props are from TaskCard.jsx
-export const EditTaskModal = ({
-	isEditModal,
-	handleEditModalClose,
-	taskId,
-	creatorPhoto,
-	creatorName,
-	refreshTasksHandle,
-}) => {
-	const [taskName, setTaskName] = useState("");
-	const [descriptionTask, setDescriptionTask] = useState("");
-	const [statusProject, setStatusProject] = useState("");
-	const [priorityLevel, setPriorityLevel] = useState("");
-	const [dueDate, setDueDate] = useState("");
+export const EditTaskModal = ({ isEditModal, handleEditModalClose, taskId, creatorPhoto, creatorName, refreshTasksHandle, }) => {
+
+	const form = useForm();
+	const { register, handleSubmit, reset, formState } = form;
+	const { errors } = formState;
+
+	const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
 
 	// retrieving current content from database
 	useEffect(() => {
@@ -26,12 +21,14 @@ export const EditTaskModal = ({
 				const docRef = doc(db, "tasks", taskId);
 				const docSnap = await getDoc(docRef);
 				if (docSnap.exists()) {
-					const data = docSnap.data();
-					setTaskName(data.taskName);
-					setDescriptionTask(data.descriptionTask);
-					setStatusProject(data.statusProject);
-					setPriorityLevel(data.priorityLevel);
-					setDueDate(data.dueDate);
+					const taskData = docSnap.data();
+					let defaultValues = {};
+					defaultValues.taskname = taskData.taskName;
+					defaultValues.taskdescription = taskData.descriptionTask;
+					defaultValues.taskstatus = taskData.statusProject;
+					defaultValues.taskpriority = taskData.priorityLevel;
+					defaultValues.taskduedate = taskData.dueDate;
+					reset({ ...defaultValues });
 				}
 			} catch (error) {
 				console.log(error);
@@ -43,14 +40,14 @@ export const EditTaskModal = ({
 	}, [isEditModal]);
 
 	// updating new task content to database
-	const handleUpdate = async () => {
+	const handleUpdate = async (data) => {
 		try {
 			await updateDoc(doc(db, "tasks", taskId), {
-				taskName,
-				descriptionTask,
-				statusProject,
-				priorityLevel,
-				dueDate,
+				taskName: data.taskname,
+				descriptionTask: data.taskdescription,
+				statusProject: data.taskstatus,
+				priorityLevel: data.taskpriority,
+				dueDate: data.taskduedate
 			});
 			if (updateDoc) {
 				handleEditModalClose();
@@ -61,139 +58,160 @@ export const EditTaskModal = ({
 		}
 	};
 
-	// handling drop down menu for project status
-	const handleSetStatusProjectChange = (e) => {
-		setStatusProject(e.target.value);
-	};
-
-	// handling drop down menu for priority level
-	const handleSetPriorityLevel = (e) => {
-		setPriorityLevel(e.target.value);
-	};
-
 	return (
 		<>
 			<Modal show={isEditModal} onHide={handleEditModalClose}>
-				<Modal.Header closeButton>
-					<Modal.Title style={{ fontSize: "15px" }} className="fw-bold">
-						Edit Task
-					</Modal.Title>
-				</Modal.Header>
+				<Form onSubmit={handleSubmit(handleUpdate)} noValidate>
+					<Modal.Header closeButton>
+						<Modal.Title style={{ fontSize: "15px" }} className="fw-bold">
+							Edit Task
+						</Modal.Title>
+					</Modal.Header>
 
-				<Modal.Body style={{ fontSize: "11px" }}>
-					<Form.Group className="mb-3" controlId="progress">
-						<Form.Label className="fw-bold" style={{ margin: "2px" }}>
-							Current Task Name
-						</Form.Label>
-						<Form.Control
-							style={{ fontSize: "10px" }}
-							type="text"
-							value={taskName}
-							onChange={(e) => setTaskName(e.target.value)}
-						/>
-					</Form.Group>
+					<Modal.Body style={{ fontSize: "11px" }}>
+						<Form.Group className="mb-3">
+							<Form.Label className="fw-bold" style={{ margin: "2px" }}>
+								Current Task Name
+							</Form.Label>
+							<Form.Control
+								style={{ fontSize: "10px" }}
+								type="text"
+								id="taskname"
+								{...register("taskname", {
+									required: {
+										value: true,
+										message: "Task name is required!"
+									}
+								})}
+							/>
+							<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.taskname?.message}</p>
+						</Form.Group>
 
-					<Form.Group className="mb-3" controlId="progress">
-						<Form.Label className="fw-bold" style={{ margin: "2px" }}>
-							Description of Task
-						</Form.Label>
-						<Form.Control
-							style={{ fontSize: "10px" }}
-							type="text"
-							as="textarea"
-							rows={3}
-							value={descriptionTask}
-							onChange={(e) => setDescriptionTask(e.target.value)}
-						/>
-					</Form.Group>
+						<Form.Group className="mb-3">
+							<Form.Label className="fw-bold" style={{ margin: "2px" }}>
+								Description of Task
+							</Form.Label>
+							<Form.Control
+								style={{ fontSize: "10px" }}
+								type="text"
+								as="textarea"
+								rows={3}
+								id="taskdescription"
+								{...register("taskdescription", {
+									required: {
+										value: true,
+										message: "Task description is required!"
+									}
+								})}
+							/>
+							<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.taskdescription?.message}</p>
+						</Form.Group>
 
-					<Form.Group className="mb-3" controlId="progress">
-						<Form.Label className="fw-bold" style={{ margin: "2px" }}>
-							Status of Project
-						</Form.Label>
-						<Form.Select
-							style={{ fontSize: "10px" }}
-							aria-label="Default select example"
-							value={statusProject}
-							onChange={handleSetStatusProjectChange}
+						<Form.Group className="mb-3">
+							<Form.Label className="fw-bold" style={{ margin: "2px" }}>
+								Status of Project
+							</Form.Label>
+							<Form.Select
+								style={{ fontSize: "10px" }}
+								aria-label="Default select example"
+								id="taskstatus"
+								{...register("taskstatus", {
+									required: true,
+									message: "Status must be chosen!"
+								})}
+							>
+								<option value="">Select status options</option>
+								<option value="On Hold">On Hold</option>
+								<option value="In Progress">In Progress</option>
+								<option value="Done">Done</option>
+								<option value="Cancelled">Cancelled</option>
+							</Form.Select>
+							<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.taskstatus?.message}</p>
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="progress">
+							<Form.Label style={{ fontSize: "10px" }} className="fw-bold">
+								What is the priority level of this project?
+							</Form.Label>
+							<Form.Select
+								style={{ fontSize: "10px" }}
+								aria-label="Default select example"
+								id="taskpriority"
+								{...register("taskpriority", {
+									required: true,
+									message: "Priority must be chosen!"
+								})}
+							>
+								<option value="">Select priority level options</option>
+								<option value="Urgent">Urgent</option>
+								<option value="High">High</option>
+								<option value="Medium">Medium</option>
+								<option value="Low">Low</option>
+							</Form.Select>
+							<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.taskpriority?.message}</p>
+						</Form.Group>
+
+						<Form.Group className="mb-3">
+							<Form.Label className="fw-bold" style={{ margin: "2px" }}>
+								Due Date
+							</Form.Label>
+							<Form.Control
+								style={{ fontSize: "10px" }}
+								type="text"
+								id="taskduedate"
+								{...register("taskduedate", {
+									required: {
+										value: true,
+										message: "Due date is required!"
+									},
+									pattern: {
+										value: dateRegex,
+										message: "Invalid date format"
+									}
+								})}
+							/>
+							<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.taskduedate?.message}</p>
+						</Form.Group>
+
+						<Stack direction="horizontal">
+							<Image
+								style={{
+									height: "35px",
+									width: "35px",
+									objectFit: "cover",
+									borderRadius: "50%",
+								}}
+								src={creatorPhoto} // user photo of task creator
+								roundedCircle
+							/>
+							<p style={{ fontSize: "10px" }} className="mt-3 ms-2">
+								Created by: {creatorName}
+							</p>
+						</Stack>
+					</Modal.Body>
+
+					<Modal.Footer>
+						<Button
+							style={{ fontSize: "10px", maxHeight: "30px" }}
+							className="ms-2"
+							variant="secondary"
+							size="sm"
+							onClick={handleEditModalClose}
 						>
-							<option value="">Select status options</option>
-							<option value="On Hold">On Hold</option>
-							<option value="In Progress">In Progress</option>
-							<option value="Done">Done</option>
-							<option value="Cancelled">Cancelled</option>
-						</Form.Select>
-					</Form.Group>
+							Cancel
+						</Button>
 
-					<Form.Group className="mb-3" controlId="progress">
-						<Form.Label style={{ fontSize: "10px" }} className="fw-bold">
-							What is the priority level of this project?
-						</Form.Label>
-						<Form.Select
-							style={{ fontSize: "10px" }}
-							aria-label="Default select example"
-							value={priorityLevel}
-							onChange={handleSetPriorityLevel}
+						<Button
+							style={{ fontSize: "10px", maxHeight: "30px" }}
+							className="ms-2"
+							variant="primary"
+							size="sm"
+							type="submit"
 						>
-							<option value="">Select priority level options</option>
-							<option value="Urgent">Urgent</option>
-							<option value="High">High</option>
-							<option value="Medium">Medium</option>
-							<option value="Low">Low</option>
-						</Form.Select>
-					</Form.Group>
-
-					<Form.Group className="mb-3" controlId="progress">
-						<Form.Label className="fw-bold" style={{ margin: "2px" }}>
-							Due Date
-						</Form.Label>
-						<Form.Control
-							style={{ fontSize: "10px" }}
-							type="text"
-							value={dueDate}
-							onChange={(e) => setDueDate(e.target.value)}
-						/>
-					</Form.Group>
-
-					<Stack direction="horizontal">
-						<Image
-							style={{
-								height: "35px",
-								width: "35px",
-								objectFit: "cover",
-								borderRadius: "50%",
-							}}
-							src={creatorPhoto} // user photo of task creator
-							roundedCircle
-						/>
-						<p style={{ fontSize: "10px" }} className="mt-3 ms-2">
-							Created by: {creatorName}
-						</p>
-					</Stack>
-				</Modal.Body>
-
-				<Modal.Footer>
-					<Button
-						style={{ fontSize: "10px", maxHeight: "30px" }}
-						className="ms-2"
-						variant="secondary"
-						size="sm"
-						onClick={handleEditModalClose}
-					>
-						Cancel
-					</Button>
-
-					<Button
-						style={{ fontSize: "10px", maxHeight: "30px" }}
-						className="ms-2"
-						variant="primary"
-						size="sm"
-						type="submit"
-						onClick={handleUpdate}
-					>
-						Update
-					</Button>
-				</Modal.Footer>
+							Update
+						</Button>
+					</Modal.Footer>
+				</Form>
 			</Modal>
 		</>
 	);
