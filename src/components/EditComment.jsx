a36b6ceb45db178fa154668e9fc878358ef4fbb4
Mitra Singh <mitra.coder@gmail.com>
2023-09-06 20:41:58 -0400

    component form updated to react form hook

--- src/components/EditComment.jsx
+++ src/components/EditComment.jsx
@@ -1,20 +1,28 @@
 import { Form, Stack, Button } from "react-bootstrap";
 import PropTypes from "prop-types";
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { db } from "../utils/firebase-config";
 import { doc, updateDoc } from "firebase/firestore";
 import { TopicIdContext } from "../utils/TopicIdContext";
+import { useForm } from "react-hook-form";
 
 export const EditComment = ({ userComment, setIsEditComment, commentId }) => {
-	const [userInput, setUserInput] = useState(userComment);
 
+	// const [userInput, setUserInput] = useState(userComment);
 	const { setIsCommentsRefreshed } = useContext(TopicIdContext);
 
-	const handleUpdateButton = async (e) => {
-		e.preventDefault();
+	const form = useForm({
+		defaultValues: {
+			editcomment: userComment
+		}
+	})
+	const { register, handleSubmit, formState } = form;
+	const { errors } = formState;
+
+	const handleUpdateButton = async (data) => {
 		try {
 			await updateDoc(doc(db, "comments", commentId), {
-				userComment: userInput,
+				userComment: data.editcomment,
 			});
 			if (updateDoc) {
 				setIsEditComment(false);
@@ -27,8 +35,8 @@ export const EditComment = ({ userComment, setIsEditComment, commentId }) => {
 
 	return (
 		<>
-			<Form className="mt-4">
-				<Form.Group className="mb-3" controlId="description">
+			<Form className="mt-4" onSubmit={handleSubmit(handleUpdateButton)} noValidate>
+				<Form.Group className="mb-3">
 					<Form.Control
 						style={{ fontSize: "10px" }}
 						maxLength={100000}
@@ -36,34 +44,43 @@ export const EditComment = ({ userComment, setIsEditComment, commentId }) => {
 						type="text"
 						as="textarea"
 						placeholder="What are your thoughts?"
-						value={userInput}
-						onChange={(e) => setUserInput(e.target.value)}
+						id="editcomment"
+						{...register("editcomment", {
+							required: {
+								value: true,
+								message: "This field cannot be empty!"
+							}
+						})}
+					// value={userInput}
+					// onChange={(e) => setUserInput(e.target.value)}
 					/>
 				</Form.Group>
+
+				<Stack direction="horizontal" gap={1}>
+					<Button
+						style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
+						className="ms-2"
+						variant="dark"
+						size="sm"
+						type="submit"
+						onClick={() => setIsEditComment(false)}
+					>
+						Cancel
+					</Button>
+					<Button
+						style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
+						className="ms-2"
+						variant="dark"
+						size="sm"
+						type="submit"
+					// onClick={handleUpdateButton}
+					>
+						Update
+					</Button>
+				</Stack>
+				<p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.editcomment?.message}</p>
 			</Form>
 
-			<Stack direction="horizontal" gap={1}>
-				<Button
-					style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
-					className="ms-2"
-					variant="dark"
-					size="sm"
-					type="submit"
-					onClick={() => setIsEditComment(false)}
-				>
-					Cancel
-				</Button>
-				<Button
-					style={{ fontSize: "10px", maxHeight: "30px", minWidth: "40px" }}
-					className="ms-2"
-					variant="dark"
-					size="sm"
-					type="submit"
-					onClick={handleUpdateButton}
-				>
-					Update
-				</Button>
-			</Stack>
 		</>
 	);
 };
