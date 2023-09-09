f14f751dd9624d7d9116adee1ed44f0babb287d4
Mitra Singh <mitra.coder@gmail.com>
2023-09-08 20:42:48 -0400

    react hook form has been implemented

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,191 +9,276 @@ import { getAuth, updateProfile, updateEmail } from "firebase/auth"
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from "../utils/firebase-config"
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
-
-
-
+import { useForm } from "react-hook-form";
 
 export const Profile = () => {
-
     // NEED TO REFACTOR UPDATE FUNCTIONALITY AS THERE IS SOME CONFUSION WITH REDUX/DATABASE UPDATING CONTENT
     // HAVE TO INCLUDE IF STATEMENTS FOR FUNCTIONALITY TO WORK
 
-    const user = useSelector((state) => state.user)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const auth = getAuth()
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const auth = getAuth();
 
-    const [userUpdatedPhoto, setUserUpdatedPhoto] = useState("") //allows user to see how photo is displayed before upload
-    const [photoURL, setPhotoURL] = useState("") 
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [title, setTitle] = useState("")
-    const [email, setEmail] = useState("")
-    
-    const [checkPhoto, setCheckPhoto] = useState(false)
+    const form = useForm();
+    const { register, handleSubmit, reset, formState } = form;
+    const { errors } = formState;
 
-    const storage = getStorage()
-    const storageRef = ref(storage)
+    const [userUpdatedPhoto, setUserUpdatedPhoto] = useState(""); //allows user to see how photo is displayed before upload
+    const [photoURL, setPhotoURL] = useState("");
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [title, setTitle] = useState("");
+    // const [email, setEmail] = useState("");
+
+    const [checkPhoto, setCheckPhoto] = useState(false);
+
+    const storage = getStorage();
+    const storageRef = ref(storage);
+
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     // updates useState values to user initial state from redux
     useEffect(() => {
-        const handleUpdateToLocal = () => {
-            setPhotoURL(user.userPhoto)
-            setFirstName(user.firstName)
-            setLastName(user.lastName)
-            setTitle(user.title)
-            setEmail(user.email)
-        }
-        handleUpdateToLocal()
-    },[])
+        const fetchUserRedux = () => {
+            let defaultValues = {};
+            defaultValues.firstname = user.firstName;
+            defaultValues.lastname = user.lastName;
+            defaultValues.title = user.title;
+            defaultValues.email = user.email;
+            reset({ ...defaultValues });
+            setPhotoURL(user.userPhoto);
+            // setFirstName(user.firstName);
+            // setLastName(user.lastName);
+            // setTitle(user.title);
+            // setEmail(user.email);
+        };
+        fetchUserRedux();
+    }, []);
 
     const setPhotoHandle = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
         try {
-            setCheckPhoto(true)
-            const imageRef = ref(storageRef, "user-photo/temp")
-            await uploadBytes(imageRef, userUpdatedPhoto)
-            const getURL = await getDownloadURL(imageRef)
-            setPhotoURL(getURL)
+            setCheckPhoto(true);
+            const imageRef = ref(storageRef, "user-photo/temp");
+            await uploadBytes(imageRef, userUpdatedPhoto);
+            const getURL = await getDownloadURL(imageRef);
+            setPhotoURL(getURL);
         } catch (error) {
-            console.log(error.code)
+            console.log(error.code);
         }
-    }
+    };
 
-    const handleUpdate = async (e) => {
-        e.preventDefault()
+    const handleUpdate = async (data) => {
         try {
             await updateProfile(auth.currentUser, {
-                displayName: firstName
-            })
-            
-            await updateDoc(doc(db, "users", auth.currentUser.uid),{
-                firstname: firstName,
-                lastname: lastName,
-                title: title
-            }) 
+                displayName: data.firstname,
+            });
+
+            await updateDoc(doc(db, "users", auth.currentUser.uid), {
+                firstname: data.firstname,
+                lastname: data.lastname,
+                title: data.title,
+            });
 
             if (checkPhoto) {
-                const imageRef = ref(storageRef, `user-photo/${auth.currentUser.uid}`)
-                await uploadBytes(imageRef, userUpdatedPhoto)
-                await getDownloadURL(ref(storageRef, `user-photo/${auth.currentUser.uid}`))
+                const imageRef = ref(storageRef, `user-photo/${auth.currentUser.uid}`);
+                await uploadBytes(imageRef, userUpdatedPhoto);
+                await getDownloadURL(
+                    ref(storageRef, `user-photo/${auth.currentUser.uid}`)
+                );
             }
 
-            await updateEmail(auth.currentUser, email)
-            
+            await updateEmail(auth.currentUser, data.email);
+
             if (updateProfile || checkPhoto || updateEmail || updateDoc) {
-                dispatch(editUser({
-                    userId: user.userId,
-                    userPhoto: checkPhoto ? photoURL : user.userPhoto,
-                    firstName: firstName,
-                    lastName: lastName,
-                    title: title,
-                    email: email
-                }))
-            }          
-            navigate("/")
+                dispatch(
+                    editUser({
+                        userId: user.userId,
+                        userPhoto: checkPhoto ? photoURL : user.userPhoto,
+                        firstName: data.firstname,
+                        lastName: data.lastname,
+                        title: data.title,
+                        email: data.email,
+                    })
+                );
+            }
+            navigate("/");
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
     return (
         <>
-        <Container className="mt-4">
-            <Row className="mb-4">
-                <Col xs lg="2">
-                    <Stack direction="vertical">
-                        <Image
-                            style={{
-                                height: "180px",
-                                width: "180px",
-                                objectFit: "cover",
-                                borderRadius: "50%"
-                            }} 
-                            // adding timestamp to bypass browser cache on image reload
-                            src={photoURL}
-                            roundedCircle />
+            <Container className="mt-4">
+                <Form onSubmit={handleSubmit(handleUpdate)} noValidate>
+                    <Row className="mb-4">
+                        <Col xs lg="2">
+                            <Stack direction="vertical">
+                                <Image
+                                    style={{
+                                        height: "180px",
+                                        width: "180px",
+                                        objectFit: "cover",
+                                        borderRadius: "50%",
+                                    }}
+                                    // adding timestamp to bypass browser cache on image reload
+                                    src={photoURL}
+                                    roundedCircle
+                                />
 
-                        <Form.Group>
-                            <Form.Control 
-                                type="file"
-                                size="sm"
-                                onChange={(event) => setUserUpdatedPhoto(event.target.files[0])}
-                            />
-                        </Form.Group>
+                                <Form.Group>
+                                    <Form.Control
+                                        type="file"
+                                        size="sm"
+                                        onChange={(event) =>
+                                            setUserUpdatedPhoto(event.target.files[0])
+                                        }
+                                    />
+                                </Form.Group>
 
-                        <Button 
-                            style={{fontSize: "8px", maxHeight: "30px", maxWidth: "75px"}} 
-                            className="ms-5 mt-2" 
-                            variant="secondary" 
-                            size="sm" 
-                            type="file"
-                            onClick={setPhotoHandle}
-                            >Set Photo
-                        </Button>
-                    </Stack>
-                </Col>
-            </Row>
+                                <Button
+                                    style={{
+                                        fontSize: "8px",
+                                        maxHeight: "30px",
+                                        maxWidth: "75px",
+                                    }}
+                                    className="ms-5 mt-2"
+                                    variant="secondary"
+                                    size="sm"
+                                    type="file"
+                                    onClick={setPhotoHandle}
+                                >
+                                    Set Photo
+                                </Button>
+                            </Stack>
+                        </Col>
+                    </Row>
 
-            <Row>
-                <Stack>
-                    <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>First Name</p>
-                    <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)} />
-                    </Form.Group>
-                    
-                    <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>Last Name</p>
-                    <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)} />
-                    </Form.Group>
-                    
-                    <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>Title</p>
-                    <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)} />
-                    </Form.Group>
-                   
-                    <p className="fw-bold" style={{fontSize: "10px", margin: "0px"}}>E-mail</p>
-                    <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)} />
-                    </Form.Group>
-                </Stack>
-            </Row>
+                    <Row>
+                        <Stack>
+                            <p
+                                className="fw-bold"
+                                style={{ fontSize: "10px", margin: "0px" }}
+                            >
+                                First Name
+                            </p>
+                            <Form.Group className="mb-3">
+                                <Form.Control
+                                    style={{ fontSize: "10px" }}
+                                    type="text"
+                                    id="firstname"
+                                    {...register("firstname", {
+                                        required: {
+                                            value: true,
+                                            message: "First name is required!"
+                                        }
+                                    })}
+                                // value={firstName}
+                                // onChange={(e) => setFirstName(e.target.value)}
+                                />
+                                <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.firstname?.message}</p>
+                            </Form.Group>
 
-            <Button 
-                style={{fontSize: "10px", maxHeight: "30px"}} 
-                variant="secondary" 
-                size="sm" 
-                as={Link} to="/" >
-                Cancel
-            </Button>
+                            <p
+                                className="fw-bold"
+                                style={{ fontSize: "10px", margin: "0px" }}
+                            >
+                                Last Name
+                            </p>
+                            <Form.Group className="mb-3">
+                                <Form.Control
+                                    style={{ fontSize: "10px" }}
+                                    type="text"
+                                    id="lastname"
+                                    {...register("lastname", {
+                                        required: {
+                                            value: true,
+                                            message: "Last name is required!"
+                                        }
+                                    })}
+                                // value={lastName}
+                                // onChange={(e) => setLastName(e.target.value)}
+                                />
+                                <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.lastname?.message}</p>
+                            </Form.Group>
 
-            <Button 
-                style={{fontSize: "10px", maxHeight: "30px"}} 
-                className="ms-2" 
-                variant="primary" 
-                size="sm" 
-                type="submit"
-                onClick={handleUpdate}>
-                Update
-            </Button>
-        </Container>
+                            <p
+                                className="fw-bold"
+                                style={{ fontSize: "10px", margin: "0px" }}
+                            >
+                                Title
+                            </p>
+                            <Form.Group className="mb-3">
+                                <Form.Control
+                                    style={{ fontSize: "10px" }}
+                                    type="text"
+                                    id="title"
+                                    {...register("title", {
+                                        required: {
+                                            value: true,
+                                            message: "Company title is required!"
+                                        }
+                                    })}
+                                // value={title}
+                                // onChange={(e) => setTitle(e.target.value)}
+                                />
+                                <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.title?.message}</p>
+                            </Form.Group>
+
+                            <p
+                                className="fw-bold"
+                                style={{ fontSize: "10px", margin: "0px" }}
+                            >
+                                E-mail
+                            </p>
+                            <Form.Group className="mb-3">
+                                <Form.Control
+                                    style={{ fontSize: "10px" }}
+                                    type="text"
+                                    id="email"
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: "Email is required!"
+                                        },
+                                        pattern: {
+                                            value: emailRegex,
+                                            message: "Invalid email"
+                                        }
+                                    })}
+                                // value={email}
+                                // onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.email?.message}</p>
+                            </Form.Group>
+                        </Stack>
+                    </Row>
+
+                    <Button
+                        style={{ fontSize: "10px", maxHeight: "30px" }}
+                        variant="secondary"
+                        size="sm"
+                        as={Link}
+                        to="/"
+                    >
+                        Cancel
+                    </Button>
+
+                    <Button
+                        style={{ fontSize: "10px", maxHeight: "30px" }}
+                        className="ms-2"
+                        variant="primary"
+                        size="sm"
+                        type="submit"
+                    // onClick={handleUpdate}
+                    >
+                        Update
+                    </Button>
+                </Form>
+            </Container>
         </>
-    )
-}
+    );
+};
 
