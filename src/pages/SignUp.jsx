285f804bffa52f29c0adb9e72378597e6e902291
Mitra Singh <mitra.coder@gmail.com>
2023-09-15 20:11:29 -0400

    implemented react hook form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,165 +1,279 @@
 import { createUserWithEmailAndPassword, updateProfile, getAuth } from "firebase/auth"
 import { doc, setDoc, getDoc } from "firebase/firestore"
 import { db } from "../utils/firebase-config"
-import { useState } from "react"
-import { Container, Form, Card, Button, Alert } from "react-bootstrap"
+// import { useState } from "react"
+import { Container, Form, Card, Button } from "react-bootstrap"
 import { useDispatch } from "react-redux"
 import { loginUser } from "../features/user/userSlice"
 import { Link, useNavigate } from "react-router-dom"
-
+import { useForm } from "react-hook-form";
 
 export const SignUp = () => {
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [title, setTitle] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+    // const [verifyPassword, setVerifyPassword] = useState("");
 
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [title, setTitle] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [verifyPassword, setVerifyPassword] = useState("")
-    
-    const [alert, setAlert] = useState(false)
-    const [alertMessage, setAlertMessage] = useState("")
+    // const [alert, setAlert] = useState(false);
+    // const [alertMessage, setAlertMessage] = useState("");
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    
-    const auth = getAuth()
-    
-    const handleSignUp = async (event) => {
-        event.preventDefault();
+    const form = useForm({ mode: "onChange" });
+    const { register, handleSubmit, watch, formState } = form;
+    const { errors, isDirty, isValid } = formState;
+
+    const watchPassword = watch("password")
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const auth = getAuth();
+
+    // const handleSignUp = (data) => {
+    //     console.log(data)
+    // }
+
+    const handleSignUp = async (data) => {
+        // event.preventDefault();
+        const { firstname, lastname, title, email, password } = data;
         try {
-            if (password === verifyPassword) {
-                await createUserWithEmailAndPassword(auth, email, password)
-                await updateProfile(auth.currentUser, {
-                    displayName: firstName,
-                })
-                await setDoc(doc(db,"users", `${auth.currentUser.uid}`), {
-                    firstname: firstName, // allows access for current auth user's name to be available throughout app
-                    lastname: lastName,
-                    title: title,
-                })
+            await createUserWithEmailAndPassword(auth, email, password);
+            await updateProfile(auth.currentUser, {
+                displayName: firstname,
+            });
+            await setDoc(doc(db, "users", `${auth.currentUser.uid}`), {
+                firstname: firstname, // allows access for current auth user's name to be available throughout app
+                lastname: lastname,
+                title: title,
+            });
 
-            } else {
-                setAlert(true)
-                setAlertMessage("Passwords do not match")
-            }
-            const docRef = doc(db, "users", auth.currentUser.uid)
-            const docSnap = await getDoc(docRef)
+            const docRef = doc(db, "users", auth.currentUser.uid);
+            const docSnap = await getDoc(docRef);
             if (auth && docSnap.exists()) {
-                const data = docSnap.data()
-                dispatch(loginUser({
-                    userId: auth.currentUser.uid,
-                    userPhoto: null,
-                    firstName: auth.currentUser.displayName,
-                    lastName: data.lastname,
-                    title: data.title,
-                    email: auth.currentUser.email
-                }))
+                const fetchUserData = docSnap.data();
+                dispatch(
+                    loginUser({
+                        userId: auth.currentUser.uid,
+                        userPhoto: null,
+                        firstName: auth.currentUser.displayName,
+                        lastName: fetchUserData.lastname,
+                        title: fetchUserData.title,
+                        email: auth.currentUser.email,
+                    })
+                );
             }
-            navigate("/photoupload")
+            navigate("/photoupload");
         } catch (error) {
-            setAlert(true)
-            setAlertMessage(error.code)
+            console.log(error.message)
         }
-    }
+    };
 
     return (
         <>
-        {alert ? (
+            {/* {alert ? (
                 <Alert variant="danger" onClose={() => setAlert(false)} dismissible>
                     <Alert.Heading>There is an error!</Alert.Heading>
                     <p>Reason: {alertMessage}</p>
                 </Alert>
-            ) : ( 
-                null
-            )}
-        
-        <Container style={{fontSize: "10px", maxWidth: "400px"}} className="mt-4">
-            <Form>
-                <Card className="px-4 py-4">
-                    <h4 className="fw-bold d-flex justify-content-center">Sign Up</h4>
-                    <p className="d-flex justify-content-center">Already registered?&nbsp; 
-                        <Link className="link-primary fw-bold" style={{cursor: "pointer"}} to="/signin">Sign In</Link> 
-                    </p>
+            ) : null} */}
 
-                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>First Name</p>
-                    <Form.Group className="mb-3">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            placeholder="Enter first name" 
-                            onChange={(e) => setFirstName(e.target.value)}/>
-                    </Form.Group>
+            <Container
+                style={{ fontSize: "10px", maxWidth: "400px" }}
+                className="mt-4"
+            >
+                <Form onSubmit={handleSubmit(handleSignUp)} noValidate>
+                    <Card className="px-4 py-4">
+                        <h4 className="fw-bold d-flex justify-content-center">Sign Up</h4>
+                        <p className="d-flex justify-content-center">
+                            Already registered?&nbsp;
+                            <Link
+                                className="link-primary fw-bold"
+                                style={{ cursor: "pointer" }}
+                                to="/signin"
+                            >
+                                Sign In
+                            </Link>
+                        </p>
 
-                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Last Name</p>
-                    <Form.Group className="mb-3">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            placeholder="Enter last name" 
-                            onChange={(e) => setLastName(e.target.value)}/>
-                    </Form.Group>
-
-                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Title</p>
-                    <Form.Group className="mb-3" controlId="progress">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            placeholder="Enter your company title" 
-                            onChange={(e) => setTitle(e.target.value)}/>
-                    </Form.Group>
-
-                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Email Address</p>
-                    <Form.Group className="mb-3">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="text" 
-                            placeholder="Enter email" 
-                            onChange={(e) => setEmail(e.target.value)}/>
-                    </Form.Group>
-
-                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Password</p>
-                    <Form.Group className="mb-3">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="password" 
-                            placeholder="Enter password" 
-                            onChange={(e) => setPassword(e.target.value)}/>
-                    </Form.Group>
-
-                    <p className="fw-bold mb-1" style={{fontSize: "10px", margin: "0px"}}>Confirm Password</p>
-                    <Form.Group className="mb-3">
-                        <Form.Control 
-                            style={{fontSize: "10px"}} 
-                            type="password" 
-                            placeholder="Enter password again" 
-                            onChange={(e) => setVerifyPassword(e.target.value)}/>
-                    </Form.Group>
-
-                    <Button 
-                        style={{fontSize: "10px", maxHeight: "30px"}} 
-                        variant="primary"  
-                        size="sm" 
-                        type="submit"
-                        onClick={handleSignUp}>
-                        Sign Up
-                    </Button>
-
-                    <Button 
-                        style={{fontSize: "10px", maxHeight: "30px"}}
-                        variant="secondary"
-                        className="mt-2" 
-                        size="sm"
-                        as={Link} to="/signin" 
+                        <p
+                            className="fw-bold mb-1"
+                            style={{ fontSize: "10px", margin: "0px" }}
                         >
-                        Cancel
-                    </Button>
+                            First Name
+                        </p>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                style={{ fontSize: "10px" }}
+                                type="text"
+                                placeholder="Enter first name"
+                                {...register("firstname", {
+                                    required: {
+                                        value: true,
+                                        message: "First name is required!"
+                                    }
+                                })}
+                            // onChange={(e) => setFirstName(e.target.value)}
+                            />
+                            <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.firstname?.message}</p>
+                        </Form.Group>
 
-                    <p className="d-flex justify-content-center link-primary mt-3">Forgot password?&nbsp;</p>
+                        <p
+                            className="fw-bold mb-1"
+                            style={{ fontSize: "10px", margin: "0px" }}
+                        >
+                            Last Name
+                        </p>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                style={{ fontSize: "10px" }}
+                                type="text"
+                                placeholder="Enter last name"
+                                {...register("lastname", {
+                                    required: {
+                                        value: true,
+                                        message: "Last name is required!"
+                                    }
+                                })}
+                            // onChange={(e) => setLastName(e.target.value)}
+                            />
+                            <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.lastname?.message}</p>
+                        </Form.Group>
 
-                </Card>
-            </Form>
-        </Container>
-        </>      
-    )
-}
+                        <p
+                            className="fw-bold mb-1"
+                            style={{ fontSize: "10px", margin: "0px" }}
+                        >
+                            Title
+                        </p>
+                        <Form.Group className="mb-3" controlId="progress">
+                            <Form.Control
+                                style={{ fontSize: "10px" }}
+                                type="text"
+                                placeholder="Enter your company title"
+                                {...register("title", {
+                                    required: {
+                                        value: true,
+                                        message: "Title is required!"
+                                    }
+                                })}
+                            // onChange={(e) => setTitle(e.target.value)}
+                            />
+                            <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.title?.message}</p>
+                        </Form.Group>
+
+                        <p
+                            className="fw-bold mb-1"
+                            style={{ fontSize: "10px", margin: "0px" }}
+                        >
+                            Email Address
+                        </p>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                style={{ fontSize: "10px" }}
+                                type="text"
+                                placeholder="Enter email"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: "Email is required!"
+                                    },
+                                    pattern: {
+                                        value: emailRegex,
+                                        message: "Invalid email format!"
+                                    }
+                                })}
+                            // onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.email?.message}</p>
+                        </Form.Group>
+
+                        <p
+                            className="fw-bold mb-1"
+                            style={{ fontSize: "10px", margin: "0px" }}
+                        >
+                            Password
+                        </p>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                style={{ fontSize: "10px" }}
+                                type="password"
+                                placeholder="Enter password"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: "Password is required!"
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: "Password must have at least 6 characters"
+                                    }
+                                })}
+                            // onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.password?.message}</p>
+                        </Form.Group>
+
+                        <p
+                            className="fw-bold mb-1"
+                            style={{ fontSize: "10px", margin: "0px" }}
+                        >
+                            Confirm Password
+                        </p>
+                        <Form.Group className="mb-3">
+                            <Form.Control
+                                style={{ fontSize: "10px" }}
+                                type="password"
+                                placeholder="Enter password again"
+                                {...register("passwordconfirm", {
+                                    required: {
+                                        value: true,
+                                        message: "Password is required!"
+                                    },
+                                    validate: {
+                                        passwordMatch: (fieldValue) => {
+                                            return fieldValue === watchPassword || "The passwords do not match"
+                                        }
+                                    }
+                                })}
+                            // onChange={(e) => setVerifyPassword(e.target.value)}
+                            />
+                            <p style={{ marginTop: "5px", fontSize: "10px", color: "red" }}>{errors.passwordconfirm?.message}</p>
+                        </Form.Group>
+
+                        <Button
+                            style={{ fontSize: "10px", maxHeight: "30px" }}
+                            variant="primary"
+                            size="sm"
+                            type="submit"
+                            disabled={!isDirty || !isValid}
+                        // onClick={handleSignUp}
+                        >
+                            Sign Up
+                        </Button>
+
+                        <Button
+                            style={{ fontSize: "10px", maxHeight: "30px" }}
+                            variant="secondary"
+                            className="mt-2"
+                            size="sm"
+                            as={Link}
+                            to="/signin"
+                        >
+                            Back to Sign In
+                        </Button>
+
+                        <Link
+                            className="d-flex justify-content-center link-primary mt-3"
+                            as={Link}
+                            to="/forgotpassword"
+                        >
+                            Forgot password?
+                        </Link>
+                    </Card>
+                </Form>
+            </Container>
+        </>
+    );
+};
