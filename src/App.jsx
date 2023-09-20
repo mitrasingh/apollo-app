d95cde4e26a737a89249b1cb149cf4604bc30ce2
Mitra Singh <mitra.coder@gmail.com>
2023-09-19 20:15:05 -0400

    re-structured indentation for better visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,64 @@
-import { Routes, Route } from 'react-router';
-import { Home } from './pages/Home';
-import { CreateTask } from './pages/CreateTask';
-import { Profile } from './pages/Profile';
-import { Shoutboard } from './pages/Shoutboard';
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { PhotoUpload } from './pages/PhotoUpload';
-import { TopicDetails } from './pages/TopicDetails'
-import { ForgotPassword } from './pages/ForgotPassword'
+import { Routes, Route } from "react-router";
+import { Home } from "./pages/Home";
+import { CreateTask } from "./pages/CreateTask";
+import { Profile } from "./pages/Profile";
+import { Shoutboard } from "./pages/Shoutboard";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { PhotoUpload } from "./pages/PhotoUpload";
+import { TopicDetails } from "./pages/TopicDetails";
+import { ForgotPassword } from "./pages/ForgotPassword";
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={
-        <ProtectedRoute>
-          <Home />
-        </ProtectedRoute>
-      }
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
       />
-      <Route path="createtask" element={
-        <ProtectedRoute>
-          <CreateTask />
-        </ProtectedRoute>
-      }
+      <Route
+        path="createtask"
+        element={
+          <ProtectedRoute>
+            <CreateTask />
+          </ProtectedRoute>
+        }
       />
-      <Route path="shoutboard" element={
-        <ProtectedRoute>
-          <Shoutboard />
-        </ProtectedRoute>
-      }
-      >
-      </Route>
-      <Route path="profile" element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      }
+      <Route
+        path="shoutboard"
+        element={
+          <ProtectedRoute>
+            <Shoutboard />
+          </ProtectedRoute>
+        }
+      ></Route>
+      <Route
+        path="profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
       />
-      <Route path="shoutboard/:id" element={
-        <ProtectedRoute>
-          <TopicDetails />
-        </ProtectedRoute>
-      }
+      <Route
+        path="shoutboard/:id"
+        element={
+          <ProtectedRoute>
+            <TopicDetails />
+          </ProtectedRoute>
+        }
       />
       <Route path="signin" element={<SignIn />} />
       <Route path="signup" element={<SignUp />} />
       <Route path="forgotpassword" element={<ForgotPassword />} />
       <Route path="photoupload" element={<PhotoUpload />} />
     </Routes>
-  )
+  );
 }
 
-export default App  
+export default App;
