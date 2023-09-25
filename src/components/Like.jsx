9cd6884b2f87debe8b15d1141f3d7b838db7fa6a
Mitra Singh <mitra.coder@gmail.com>
2023-09-25 12:07:38 -0400

    added comment stating parent component of props

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -5,8 +5,7 @@ import { db } from "../utils/firebase-config"
 import { useSelector } from "react-redux"
 import PropTypes from 'prop-types';
 
-
-export const Like = ({ docId }) => {
+export const Like = ({ docId }) => { // Prop from CommentCard.jsx
 
     const [likes, setLikes] = useState([])
     const currentUser = useSelector((state) => state.user)
@@ -16,10 +15,10 @@ export const Like = ({ docId }) => {
     useEffect(() => {
         const getLikes = async () => {
             const data = await getDocs(likesDoc)
-            setLikes(data.docs.map((doc) => ({userId: doc.data().userId, likeId: doc.id})))
+            setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })))
         }
         getLikes()
-    },[])
+    }, [])
 
     const addLikeHandle = async () => {
         try {
@@ -27,7 +26,7 @@ export const Like = ({ docId }) => {
                 userId: currentUser.userId,
                 docRefId: docId
             })
-            setLikes((prev) => prev ? [...prev, { userId: currentUser.userId, likeId: newDoc.id }] : [{ userId: currentUser.userId, likeId: newDoc.id}])
+            setLikes((prev) => prev ? [...prev, { userId: currentUser.userId, likeId: newDoc.id }] : [{ userId: currentUser.userId, likeId: newDoc.id }])
         } catch (error) {
             console.log(error)
         }
@@ -36,13 +35,13 @@ export const Like = ({ docId }) => {
     const removeLikeHandle = async () => {
         try {
             const likeToDeleteQuery = query(
-                likesRef, 
+                likesRef,
                 where("docRefId", "==", docId),
                 where("userId", "==", currentUser.userId)
             )
             const likeToDeleteData = await getDocs(likeToDeleteQuery)
             const likeId = likeToDeleteData.docs[0].id
-            const likeToDelete = doc(db,"likes",likeId)
+            const likeToDelete = doc(db, "likes", likeId)
             await deleteDoc(likeToDelete)
             setLikes((prev) => prev.filter((like) => like.likeId !== likeId))
         } catch (error) {
@@ -61,9 +60,9 @@ export const Like = ({ docId }) => {
                     height="20"
                     className="d-inline-block align-top"
                     alt="apollo logo"
-                    onClick={checkUserLiked ? removeLikeHandle : addLikeHandle} 
+                    onClick={checkUserLiked ? removeLikeHandle : addLikeHandle}
                 />
-                <p style={{fontSize:"9px", marginTop:"12px"}} className="mt-3">Likes: {likes.length} </p>
+                <p style={{ fontSize: "9px", marginTop: "12px" }} className="mt-3">Likes: {likes.length} </p>
             </Stack>
         </Row>
     )
