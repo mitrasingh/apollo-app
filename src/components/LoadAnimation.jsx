aee228bc47a77372eddb3a684d027826906a0f44
Mitra Singh <mitra.coder@gmail.com>
2023-09-25 18:13:19 -0400

    improve space within brackets

--- src/components/LoadAnimation.jsx
+++ src/components/LoadAnimation.jsx
@@ -1,6 +1,6 @@
 import Spinner from 'react-bootstrap/Spinner';
 
 export function LoadAnimation() {
-  return <Spinner style={{position: "fixed", top: "50%", left: "50%"}} animation="border" />;
+  return <Spinner style={{ position: "fixed", top: "50%", left: "50%" }} animation="border" />;
 }
 
