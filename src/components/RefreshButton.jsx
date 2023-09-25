0c0b240c259b6cca8e4086f5d84faf3f83641837
Mitra Singh <mitra.coder@gmail.com>
2023-09-25 18:08:22 -0400

    added comment for props

--- src/components/RefreshButton.jsx
+++ src/components/RefreshButton.jsx
@@ -1,19 +1,22 @@
 import { Button, Container } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
+// Props from Home.jsx
 export const RefreshButton = ({ refreshTasksHandle, isClearFilterDisplayed }) => {
+
   return (
     <Container>
       <Button
         style={{ fontSize: "9px", maxHeight: "20px" }}
         className="d-flex align-items-center"
         variant="dark"
-        onClick={refreshTasksHandle}>
+        onClick={refreshTasksHandle}
+      >
         {isClearFilterDisplayed ? "Clear Filters" : "Refresh Tasks"}
       </Button>
     </Container>
-  )
-}
+  );
+};
 
 RefreshButton.propTypes = {
   refreshTasksHandle: PropTypes.func,
