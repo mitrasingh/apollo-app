6177d82721ba8f96c4a4d817f27adae3681d00e4
Mitra Singh <mitra.coder@gmail.com>
2023-09-29 00:37:32 -0400

    refactored code and added commentary for better readability

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -7,67 +7,89 @@ import PropTypes from 'prop-types';
 
 export const Like = ({ docId }) => { // Prop from CommentCard.jsx
 
-    const [likes, setLikes] = useState([])
-    const currentUser = useSelector((state) => state.user)
-    const likesRef = collection(db, "likes")
-    const likesDoc = query(likesRef, where("docRefId", "==", docId))
+    const [likes, setLikes] = useState([]); // Amount of docs in likes collection that match prop docId
+    const currentUser = useSelector((state) => state.user); // User state values from Redux
 
+    // Fetch all likes documents that only match prop docId
     useEffect(() => {
         const getLikes = async () => {
-            const data = await getDocs(likesDoc)
-            setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })))
-        }
-        getLikes()
-    }, [])
+            const likesRef = collection(db, "likes");
+            const likesQuery = query(likesRef, where("docRefId", "==", docId));
+            const likesDocs = await getDocs(likesQuery);
+            const mapDocs = likesDocs.docs.map((doc) => ({
+                userId: doc.data().userId,
+                likeId: doc.id,
+            }));
+            setLikes(mapDocs);
+        };
+        getLikes();
+    }, []);
 
+    // Creates a document in likes collection
     const addLikeHandle = async () => {
         try {
-            const newDoc = await addDoc(likesRef, {
+            const likesRef = collection(db, "likes");
+            const newLikeDoc = await addDoc(likesRef, {
                 userId: currentUser.userId,
-                docRefId: docId
-            })
-            setLikes((prev) => prev ? [...prev, { userId: currentUser.userId, likeId: newDoc.id }] : [{ userId: currentUser.userId, likeId: newDoc.id }])
+                docRefId: docId,
+            });
+            const addNewLike = (prev) => [
+                ...prev,
+                { userId: currentUser.userId, likeId: newLikeDoc.id },
+            ];
+            setLikes(addNewLike);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
+    // Removes a specified document in likes collection
     const removeLikeHandle = async () => {
         try {
-            const likeToDeleteQuery = query(
+            const likesRef = collection(db, "likes");
+            const likesQuery = query(
                 likesRef,
                 where("docRefId", "==", docId),
                 where("userId", "==", currentUser.userId)
-            )
-            const likeToDeleteData = await getDocs(likeToDeleteQuery)
-            const likeId = likeToDeleteData.docs[0].id
-            const likeToDelete = doc(db, "likes", likeId)
-            await deleteDoc(likeToDelete)
-            setLikes((prev) => prev.filter((like) => like.likeId !== likeId))
+            );
+            const likesOfQryDocs = await getDocs(likesQuery);
+            const likesDocId = likesOfQryDocs.docs[0].id;
+            const likesDeleteDoc = doc(db, "likes", likesDocId);
+            await deleteDoc(likesDeleteDoc);
+            const likeFilter = (prev) =>
+                prev.filter((like) => like.likeId !== likesDocId);
+            setLikes(likeFilter);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
-    const checkUserLiked = likes.find((like) => like.userId === currentUser.userId)
+    // Verifies if user has already liked post (if user has, post will be unliked)
+    const hasUserLiked = likes.find((like) => like.userId === currentUser.userId);
 
     return (
         <Row>
             <Stack direction="horizontal" className="mt-3" gap={2}>
                 <img
-                    src={checkUserLiked ? "/public/img/rocketLike.svg" : "/public/img/rocketNoLike.svg"}
+                    src={
+                        hasUserLiked
+                            ? "/public/img/rocketLike.svg"
+                            : "/public/img/rocketNoLike.svg"
+                    }
                     width="20"
                     height="20"
                     className="d-inline-block align-top"
                     alt="apollo logo"
-                    onClick={checkUserLiked ? removeLikeHandle : addLikeHandle}
+                    onClick={hasUserLiked ? removeLikeHandle : addLikeHandle}
                 />
-                <p style={{ fontSize: "9px", marginTop: "12px" }} className="mt-3">Likes: {likes.length} </p>
+                <p style={{ fontSize: "9px", marginTop: "12px" }} className="mt-3">
+                    Likes: {likes.length}{" "}
+                </p>
             </Stack>
         </Row>
-    )
-}
+    );
+};
 
 Like.propTypes = {
-    docId: PropTypes.string
-}
+    docId: PropTypes.string.isRequired,
+};
